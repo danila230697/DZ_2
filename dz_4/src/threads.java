6dbf1baf282594b6d3a4f7a9a1ca59dba602b640
danila <danila230697@yandex.ru>
2020-07-25 20:16:15 +0300

    new DZ(4) threads

--- dz_4/src/threads.java
+++ dz_4/src/threads.java
@@ -1,7 +1,8 @@
 public class threads {
     private static final Object mon=new Object();
     private  static volatile char currentLetter='A';
-    //Задание выполнил если нужно каждый символ отдельно ну понятно тогда что вместо print пишем println
+    //Задание выполнил если нужно каждый символ отдельно
+    // ну понятно тогда что вместо print пишем println
     public static void main(String[] args) {
 threads threads=new threads();
 new Thread(()->threads.printA()).start();
